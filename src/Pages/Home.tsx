abc0520e229b7fe9f8538ce9b577bd0d0b09127e
Robbert van Markus <robbert@vanmarkus.com>
2017-10-28 16:28:34 +0200

    villa informatie homepage with markup, styling en prijs

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,17 +1,23 @@
-import { VillaCompactView } from './Villa/Villa';
+import { VillaCompactView, VillaFaciliteiten } from './Villa/Villa';
 import { PhotoSlider } from '../photoslider/Photoslider';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-
+import * as ReactMarkdown from 'react-markdown';
 import * as React from 'react';
 import './Home.css';
 import { ChateauItem, ChateauListViewComponent, ChateauPost } from './Chateau/Chateau';
-import { CHATEAU_CONTENT_TYPE_ID, ContentfulClient, SLIDER_PHOTO_CONTENT_TYPE_ID } from '../Contentful';
+import {
+  CHATEAU_CONTENT_TYPE_ID,
+  ContentfulClient,
+  SLIDER_PHOTO_CONTENT_TYPE_ID,
+  VILLAS_CONTENT_TYPE_ID,
+} from '../Contentful';
 import { ContentfulClientApi, Entry, EntryCollection } from 'contentful';
 import { Photo, SliderPhotoContentModel } from '../photoslider/Types';
 interface State {
   introClosed: boolean;
   chateauPosts?: Entry<ChateauPost>[];
   sliderPhotos?: Photo[];
+  villaFaciliteiten?: VillaFaciliteiten[];
 }
 
 export class Home extends React.Component<{}, State> {
@@ -20,16 +26,39 @@ export class Home extends React.Component<{}, State> {
     introClosed: false,
     chateauPosts: [],
     sliderPhotos: [],
+    villaFaciliteiten: []
   };
   constructor() {
     super();
     this.initPhotoSliderContentState();
     this.initChateauContentState();
+    this.initVillaFaciliteitenState();
   }
-
+  initVillaFaciliteitenState() {
+    return this.client.getEntries({
+      content_type: VILLAS_CONTENT_TYPE_ID,
+      select: ['fields.faciliteiten,fields.title,fields.prijsVanaf']
+    })
+      .then(entries => entries.items.map((villa: Entry<VillaFaciliteiten>) => {
+        return {
+          id: villa.sys.id,
+          title: villa.fields.title,
+          faciliteiten: villa.fields.faciliteiten,
+          prijsVanaf: villa.fields.prijsVanaf
+        };
+      }))
+      .then((faciliteiten) => {
+        this.setState({
+          ... this.state,
+          villaFaciliteiten: faciliteiten
+        });
+      });
+  }
   initPhotoSliderContentState() {
-    return this.client.getEntries({ content_type: SLIDER_PHOTO_CONTENT_TYPE_ID,
-      'fields.page.sys.id[all]': 'CpAKWj8P7iWggMwIkm4K2'})
+    return this.client.getEntries({
+      content_type: SLIDER_PHOTO_CONTENT_TYPE_ID,
+      'fields.page.sys.id[all]': 'CpAKWj8P7iWggMwIkm4K2'
+    })
       .then((content: EntryCollection<SliderPhotoContentModel>) => content.items.map(photo => {
         return {
           original: photo.fields.image.fields.file.url,
@@ -65,19 +94,21 @@ export class Home extends React.Component<{}, State> {
             .map((item: Entry<ChateauPost>) => <ChateauItem key={item.fields.slug} item={item} />)}
         </ChateauListViewComponent>
         <VillaCompactView>
-          <Tabs>
+          { this.state.villaFaciliteiten.length > 0 ?
+          <Tabs defaultIndex={0} >
             <TabList>
-              <Tab>Title 1</Tab>
-              <Tab>Title 2</Tab>
+             { this.state.villaFaciliteiten.map((faciliteiten: VillaFaciliteiten, i) =>
+              <Tab key={faciliteiten.id} tabIndex={i.toString()}> {faciliteiten.title}</Tab>)}
             </TabList>
 
-            <TabPanel>
-              <h2>Any content 1</h2>
-            </TabPanel>
-            <TabPanel>
-              <h2>Any content 2</h2>
-            </TabPanel>
+           { this.state.villaFaciliteiten.map((faciliteiten: VillaFaciliteiten, i) => 
+            <TabPanel key={faciliteiten.id}>
+            <ReactMarkdown source={faciliteiten.faciliteiten} />
+            <span className="prijs"><span>Vanaf </span> {faciliteiten.prijsVanaf} <span> per week</span></span>
+            <button className="button yellow">Villa informatie & beschikbaarheid</button>
+           </TabPanel>)}
           </Tabs>
+          : ''}
         </VillaCompactView>
         <section>Villa</section>
         <section>Omgeving</section>
--- src/Pages/Villa/Villa.tsx
+++ src/Pages/Villa/Villa.tsx
@@ -17,11 +17,18 @@ export class VillaCompactView extends React.Component<Props, State> {
     }
 }
 export interface VillaContentModel {
+    id: string;
     title: string;
     faciliteiten: string;
     description: string;
     plattegrond: ContentfulPhoto;
     infoRechts: string;
+    prijsVanaf: string;
+}
+export interface VillaFaciliteiten extends Partial<VillaContentModel> {
+    id: string;
+    title: string;
+    faciliteiten: string;
     prijsVanaf: string;
 }
 export const villa: React.StatelessComponent<{faciliteiten: string}> = props => {
